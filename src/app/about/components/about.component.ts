337aac93d713fc977fe1601532637ffe4507d444
lap nguyen <lap nguyen>
2016-12-30 10:35:12 +0700

    Read me

---
+++ src/app/about/components/about.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+import {OnInit} from "@angular/core";
+@Component({
+    templateUrl: './app/about/components/about.html',
+    styleUrls: ['./app/about/components/about.css'],
+})
+
+
+export class AboutComponent implements OnInit {
+    ngOnInit() {
+        //ooo oo ppppp oopp uu uuu oo pp sdas ooo
+    }
+}
